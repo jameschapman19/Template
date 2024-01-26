70f650674d86e8904b6bfdfded51bc721927d1bd
jameschapman19 <james.chapman.19@ucl.ac.uk>
2024-01-26 15:39:06 +0000

    Update flask bit

--- api/index.py
+++ api/index.py
@@ -1,18 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/api/process', methods=['POST'])
 def process_files():
-    files = request.files.getlist('files')
-    query = request.form.get('query')
-
-    # TODO: Process files and query here
-    # Example: Just return the filenames and query
-    filenames = [file.filename for file in files]
-    return jsonify({"filenames": filenames, "query": query})
+    return jsonify({"message": "Hello from Flask!"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5328)
